c09105e7c5f8b482ffcf811dbc735468b40c0e66
Guru Kathiresan <gurukathiresan@hotmail.com>
2019-09-12 15:49:09 -0700

    Initial code upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbValluvarLine = new System.Windows.Forms.Label();
+            this.lbLathaLine = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Valluvar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Latha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("TM-TTValluvar", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(401, 50);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(99, 474);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.Visible = false;
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.RichTextBox1_SelectionChanged);
+            this.richTextBox1.VScroll += new System.EventHandler(this.RichTextBox1_VScroll);
+            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RichTextBox1_KeyPress);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(523, 49);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(98, 475);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            this.richTextBox2.Visible = false;
+            this.richTextBox2.SelectionChanged += new System.EventHandler(this.RichTextBox2_SelectionChanged);
+            this.richTextBox2.VScroll += new System.EventHandler(this.RichTextBox2_VScroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 518);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeight = 29;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Valluvar,
+            this.Latha});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 60;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(357, 581);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Valluvar Symbols";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(520, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Latha Symbols";
+            this.label3.Visible = false;
+            // 
+            // lbValluvarLine
+            // 
+            this.lbValluvarLine.AutoSize = true;
+            this.lbValluvarLine.Location = new System.Drawing.Point(398, 527);
+            this.lbValluvarLine.Name = "lbValluvarLine";
+            this.lbValluvarLine.Size = new System.Drawing.Size(116, 17);
+            this.lbValluvarLine.TabIndex = 8;
+            this.lbValluvarLine.Text = "Valluvar Symbols";
+            this.lbValluvarLine.Visible = false;
+            // 
+            // lbLathaLine
+            // 
+            this.lbLathaLine.AutoSize = true;
+            this.lbLathaLine.Location = new System.Drawing.Point(520, 527);
+            this.lbLathaLine.Name = "lbLathaLine";
+            this.lbLathaLine.Size = new System.Drawing.Size(101, 17);
+            this.lbLathaLine.TabIndex = 9;
+            this.lbLathaLine.Text = "Latha Symbols";
+            this.lbLathaLine.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(376, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Valluvar Text";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("TM-TTValluvar", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox3.Location = new System.Drawing.Point(401, 50);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(743, 207);
+            this.richTextBox3.TabIndex = 11;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.RichTextBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(376, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Latha Text";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(401, 300);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.Size = new System.Drawing.Size(743, 269);
+            this.richTextBox4.TabIndex = 13;
+            this.richTextBox4.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(469, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 27);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Valluvar
+            // 
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("TM-TTValluvar", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Valluvar.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Valluvar.HeaderText = "Valluvar";
+            this.Valluvar.MinimumWidth = 6;
+            this.Valluvar.Name = "Valluvar";
+            this.Valluvar.Width = 90;
+            // 
+            // Latha
+            // 
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Latha", 7.8F);
+            this.Latha.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Latha.HeaderText = "Latha";
+            this.Latha.MinimumWidth = 6;
+            this.Latha.Name = "Latha";
+            this.Latha.Width = 90;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1008, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 27);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Copy to Clipboard";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(561, 267);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(427, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 16;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 581);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbLathaLine);
+            this.Controls.Add(this.lbValluvarLine);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbValluvarLine;
+        private System.Windows.Forms.Label lbLathaLine;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Valluvar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Latha;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor
+{
+    public partial class Form1 : Form
+    {
+        public enum ScrollBarType : uint
+        {
+            SbHorz = 0,
+            SbVert = 1,
+            SbCtl = 2,
+            SbBoth = 3
+        }
+
+        public enum Message : uint
+        {
+            WM_VSCROLL = 0x0115
+        }
+
+        public enum ScrollBarCommands : uint
+        {
+            SB_THUMBPOSITION = 4
+        }
+
+        [DllImport("User32.dll")]
+        public extern static int GetScrollPos(IntPtr hWnd, int nBar);
+
+        [DllImport("User32.dll")]
+        public extern static int SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            richTextBox1.Text = richTextBox1.Text.Trim();
+            richTextBox2.Text = richTextBox2.Text.Trim();
+
+            //
+            var lines = richTextBox1.Lines.ToList();
+            var linesToCopy = new List<string>();
+            foreach (var line in lines)
+            {
+                if (line.Trim() == "")
+                    continue;
+                linesToCopy.Add(line);
+            }
+            richTextBox1.Lines = linesToCopy.ToArray();
+
+            //
+            lines = richTextBox2.Lines.ToList();
+            linesToCopy.Clear();
+            foreach (var line in lines)
+            {
+                if (line.Trim() == "")
+                    continue;
+                linesToCopy.Add(line);
+            }
+            richTextBox2.Lines = linesToCopy.ToArray();
+
+            Button1_Click(null, null);
+
+            UpdateLineCounts();
+        }
+
+        private void ListView1_DrawItem(object sender, DrawListViewItemEventArgs e)
+        {
+            using (StringFormat sf = new StringFormat())
+            {
+                // Draw the standard header background.
+                e.DrawBackground();
+                var fontName = "TM-TTValluvar";
+                if (e.ItemIndex == 1)
+                {
+                    fontName = "Latha";
+                }
+
+                // Draw the header text.
+                using (Font headerFont =
+                            new Font(fontName, 10, FontStyle.Bold)) //Font size!!!!
+                {
+                    e.Graphics.DrawString(e.Item.Text, headerFont,
+                        Brushes.Black, e.Bounds, sf);
+                }
+            }
+            return;
+        }
+
+        private void UpdateLineCounts()
+        {
+            int cursorPosition = richTextBox1.SelectionStart;
+            int lineIndex = richTextBox1.GetLineFromCharIndex(cursorPosition) + 1;
+            lbValluvarLine.Text = "Line - " + lineIndex.ToString();
+
+            cursorPosition = richTextBox2.SelectionStart;
+            lineIndex = richTextBox2.GetLineFromCharIndex(cursorPosition) + 1;
+            lbLathaLine.Text = "Line - " + lineIndex.ToString();
+        }
+
+        private void RichTextBox1_VScroll(object sender, EventArgs e)
+        {
+                int nPos = GetScrollPos(richTextBox1.Handle, (int)ScrollBarType.SbVert);
+                nPos <<= 16;
+                uint wParam = (uint)ScrollBarCommands.SB_THUMBPOSITION | (uint)nPos;
+                SendMessage(richTextBox2.Handle, (int)Message.WM_VSCROLL, new IntPtr(wParam), new IntPtr(0));
+
+            UpdateLineCounts();
+        }
+
+        private void RichTextBox2_VScroll(object sender, EventArgs e)
+        {
+            UpdateLineCounts();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //listView1.Items.Clear();
+
+            if (richTextBox1.Lines.Count() != richTextBox2.Lines.Count())
+            {
+                MessageBox.Show($"Line count does not match - richTextBox1 = {richTextBox1.Lines.Count()}  - richTextBox2 = {richTextBox2.Lines.Count()}, adjust the characters properly");
+                //return;
+            }
+
+            if (richTextBox2.Lines.Count() > richTextBox1.Lines.Count())
+            {
+                var lines = richTextBox1.Lines.ToList();
+                for (int i = 0; i <  richTextBox2.Lines.Count() - richTextBox1.Lines.Count();++i)
+                {
+                    lines.Add("");
+                }
+                richTextBox1.Lines = lines.ToArray();
+            }
+            else if (richTextBox1.Lines.Count() > richTextBox2.Lines.Count())
+            {
+                var lines = richTextBox2.Lines.ToList();
+                for (int i = 0; i < richTextBox1.Lines.Count() - richTextBox2.Lines.Count(); ++i)
+                {
+                    lines.Add("");
+                }
+                richTextBox2.Lines = lines.ToArray();
+            }
+
+            dataGridView1.Rows.Clear();
+            //listView1.Items.Clear();
+            //listView1.Font = new Font("TM -TTValluvar", 10, FontStyle.Bold);
+
+            for(var i = 0; i < richTextBox1.Lines.Count(); ++i)
+            {
+                var line1 = richTextBox1.Lines[i].Trim();
+                var line2 = richTextBox2.Lines[i].Trim();
+                //var item = listView1.Items.Add(line1.Trim());
+                //item.SubItems.Add(line2.Trim());
+                //item.Font = new Font("Latha", 10, FontStyle.Bold);
+                this.dataGridView1.Rows.Add(line1, line2);
+                this.dataGridView1.Rows[i].HeaderCell.Value = String.Format("{0}", i + 1);
+            }
+
+            //ConvertText();
+        }
+
+        private void ListView1_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+        {
+            //
+        }
+
+        private void RichTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+
+        private void RichTextBox2_SelectionChanged(object sender, EventArgs e)
+        {
+            UpdateLineCounts();
+        }
+
+        private void RichTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+            UpdateLineCounts();
+        }
+
+        private void RichTextBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void ConvertText()
+        {
+            var symbols = new List<KeyValuePair<string, string>>();
+
+            for (var i = 0; i < dataGridView1.RowCount; ++i)
+            {
+                var val0 = dataGridView1.Rows[i].Cells[0].Value;
+                var val1 = dataGridView1.Rows[i].Cells[1].Value;
+                var str0 = "";
+                var str1 = "";
+
+                if (val0 != null)
+                    str0 = val0.ToString().Trim();
+
+                if (val1 != null)
+                    str1 = val1.ToString().Trim();
+
+                symbols.Add(new KeyValuePair<string, string>(str0, str1));                
+            }
+
+            symbols =  symbols.OrderByDescending(x => x.Key.Length).ToList();
+
+            var modifiedLines = new List<string>();
+
+            progressBar1.Minimum = 0;
+            progressBar1.Value = 0;
+            progressBar1.Maximum = richTextBox3.Lines.Count();
+
+            foreach (var line in richTextBox3.Lines)
+            {
+                Application.DoEvents();
+                var modifiedLine = line;
+                foreach (var sym in symbols)
+                {
+                    if (sym.Key == "")
+                        continue;
+
+                    modifiedLine = modifiedLine.Replace(sym.Key, sym.Value);
+                }
+                modifiedLines.Add(modifiedLine);
+                progressBar1.Value = progressBar1.Value + 1;
+            }
+
+            richTextBox4.Lines = modifiedLines.ToArray();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            ConvertText();
+            progressBar1.Visible = false;
+            MessageBox.Show("Text is converted");
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            richTextBox4.SelectAll();
+            richTextBox4.Copy();
+            MessageBox.Show("Converted text is copied to clipboard");
+        }
+    }
+}
